1158976498e7d12125275fc18bb894f48b04e66d
Damith C. Rajapakse <damith@gmail.com>
2021-07-29 20:29:39 +0800

    README: Mention the project language level setting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,27 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
+        /*
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        */
+        String line;
+        Scanner in= new Scanner(System.in);
+        System.out.println("Hello! I'm Duke");
+        System.out.println("What can I do for you?");
+
+
+        while(true){
+            line=in.nextLine();
+            System.out.println(line);
+            if("bye".equalsIgnoreCase(line)){
+             break;
+            }
+        }
+        System.out.println("Bye. Hope to see you again soon!");
     }
 }
