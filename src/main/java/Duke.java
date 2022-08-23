c542bb7c206f380273f6ec8f3978eb1ea46c7038
denny-wongso <denny.wongso@gmail.com>
2022-08-23 20:30:03 +0800

    remove unused function update check keywords to ignore case

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,19 +2,14 @@ import java.util.Scanner;
 
 
 public class Duke {
-
-    public static void seperator() {
-        System.out.println("-----------------------------------------------");
-    }
-
     public static void main(String[] args) {
         Bot bot = new Bot("lamBDA", "--------------------------");
         bot.welcome();
         String line;
         Scanner in = new Scanner(System.in);
         line = in.nextLine();
-        while(line.equals("Bye") == false) {
-            if(line.equals("list")) {
+        while(line.equalsIgnoreCase("bye") == false) {
+            if(line.equalsIgnoreCase("list")) {
                 bot.showList();
             } else {
                 bot.show(line);
