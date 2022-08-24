50e973f2786d003575c14eaa0e6ce7cf4a53df61
kaiting12 <kaiting12@live.com>
2022-08-24 23:00:03 +0800

    Add Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,19 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        Scanner input = new Scanner(System.in);
+        System.out.print("Hello! I'm Duke\n"+"What can I do for you?:\n");
+        while (true) {
+            String line = input.nextLine();
+            if ("bye".equals(line)) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+            else{
+                System.out.println(line+"\n");
+            }
+        }
     }
 }
+
+
