28e9a9bc75b3ca70e51b68ff2226248521d0ff3c
cheezelx <39342938+kaiting12@users.noreply.github.com>
2022-08-24 22:53:23 +0800

    Revert "add level-1"

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,5 +1,3 @@
-import java.util.Scanner;
-
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -8,17 +6,5 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-
-        Scanner input = new Scanner(System.in);
-        System.out.print("Hello! I'm Duke\n"+"What can I do for you?:\n");
-        while (true) {
-            String line = input.nextLine();
-            if ("bye".equalsIgnoreCase(line)) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            }
-            System.out.println(line);
-        }
     }
 }
-
