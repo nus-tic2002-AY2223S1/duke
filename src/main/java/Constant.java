188d246432236c68d6048333a4879fb2130367fc
Rina Chua <rinaxchua@gmail.com>
2022-08-28 15:21:56 +0800

    Level 1 - Greet, Echo, Exit

---
+++ src/main/java/Constant.java
@@ -0,0 +1,16 @@
+public class Constant {
+    public static final String BYE = "bye";
+    public static final String BYE_GREETING = "Bye! Hope to see you again soon!";
+    public static final String DASHES = new String(new char[80]).replace("\0", "-");
+    public static final String HELLO_GREETING = "Hello! I'm Duke! How can I help you?";
+    public static final String INPUT_OPTIONS = "Input options: \n" +
+            "1. Enter Free Text \n" +
+            "2. Enter 'list' to see the list of items \n" +
+            "3. Enter 'bye' to exit";
+    public static final String LOGO = " ____        _        \n"
+            + "|  _ \\ _   _| | _____ \n"
+            + "| | | | | | | |/ / _ \\\n"
+            + "| |_| | |_| |   <  __/\n"
+            + "|____/ \\__,_|_|\\_\\___|\n";
+    public static final String PROMPT_INPUT = "Please enter your input below:";
+}
--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,75 @@
+import java.util.Scanner;
+
 public class Duke {
+    /**
+     * printLine prints line
+     *
+     * @return {void}
+     */
+    public static void printLine() {
+        System.out.println(Constant.DASHES);
+    }
+
+    /**
+     * greet prints greeting text
+     *
+     * @return {void}
+     */
+    public static void greet() {
+        printLine();
+        System.out.println(Constant.HELLO_GREETING);
+        System.out.println(Constant.INPUT_OPTIONS);
+        System.out.println(Constant.PROMPT_INPUT);
+        printLine();
+    }
+
+    /**
+     * echo prints item entered by user
+     *
+     * @param {String} item entered by user
+     * @return {void}
+     */
+    public static void echo(String input) {
+        printLine();
+        System.out.println(input);
+        printLine();
+    }
+
+    /**
+     * exit prints bye statement
+     *
+     * @return {void}
+     */
+    public static void exit() {
+        printLine();
+        System.out.println(Constant.BYE_GREETING);
+        printLine();
+    }
+
+    /**
+     * chat returns the flow of the chat
+     *
+     * @return {void}
+     */
+    public static void chat() {
+        Scanner userInput = new Scanner(System.in);
+
+        while (userInput.hasNext()) {
+            String input = (userInput.nextLine()).toLowerCase();
+            switch (input) {
+                case Constant.BYE:
+                    exit();
+                    return;
+                default:
+                    echo(input);
+            }
+        }
+    }
+
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.println(Constant.LOGO);
+
+        greet();
+        chat();
     }
 }
