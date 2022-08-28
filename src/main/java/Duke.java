32527dbf8e29ca3b7ad2dbf31f4edb6674a96000
James <jamesyuyanen@gmail.com>
2022-08-28 18:14:50 +0800

    Level-2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -10,17 +10,31 @@ public class Duke {
         System.out.println("Hello from\n" + logo);
         */
         String line;
+        String[] book_list=new String[100];
         Scanner in= new Scanner(System.in);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
 
 
+        int count=0;
+
         while(true){
             line=in.nextLine();
-            System.out.println(line);
+
+            if("read book".equalsIgnoreCase(line) || "return book".equalsIgnoreCase(line)){
+                    book_list[count]=line;
+                    count++;
+                System.out.println("added: "+line);
+                }
+
+
             if("bye".equalsIgnoreCase(line)){
              break;
-             
+            }
+            if("list".equalsIgnoreCase(line)){
+               for(int i=1;i<=count;i++){
+                   System.out.println(i+". "+book_list[i-1]);
+               }
             }
         }
         System.out.println("Bye. Hope to see you again soon!");
