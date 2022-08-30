c3dd2fb2197f56e7dea3d735aa36366bf1d6fc9e
xu kaipeng <>
2022-08-30 23:50:44 +0800

    Level 1-

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,52 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static final String LONG_LINE = "____________________________________________________________";
+
     public static void main(String[] args) {
         greeting();
+
+        Scanner in = new Scanner(System.in);
+        Boolean exit = false;
+        while(!exit){
+        String line = in.nextLine();
+        switch (line){
+            case"bye":
+                exit = true;
+                break;
+            default:
+                printLine(line);
+
+        }
+
+        }
+        in.close();
         goodbye();
     }
 
     public static void greeting() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        // add"what can i do for you"
-        System.out.println("What can i do for you?");
+        String logo = "    =    |\\    | |     | ======   |=====\n"
+                    + "   / \\   | \\   |  |   |  ||       |     =\n"
+                    + "  /___\\  |  \\  |   | |   ||====   |=====\n"
+                    + " /-----\\ |   \\ |    |    ||       |   \\\n"
+                    + "/       \\|    \\|    |    ======   |    \\\n";
+        System.out.println( logo);
+        printIndent(LONG_LINE);
+        printIndent("Hello! I am Anyer");
+        printIndent("What can I do for you?");
+        printIndent(LONG_LINE);
     }
     //add"good bye "
     public static void goodbye(){
-        System.out.println("Bye.Hope to see you again soon!");
+        printLine("Bye.Hope to see you again soon!");
+    }
 
+    protected static void printLine(String line){
+        printIndent(LONG_LINE);
+        printIndent(line);
+        printIndent(LONG_LINE);
+    }
+    protected static void printIndent(String line){
+        System.out.println("   "+line);
     }
 }
