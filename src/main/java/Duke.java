f438bd91a920c7315497028ca749f198a242dc06
xu kaipeng <>
2022-08-30 22:40:30 +0800

    greet&good bye

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,22 @@
 public class Duke {
     public static void main(String[] args) {
+        greeting();
+        goodbye();
+    }
+
+    public static void greeting() {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+        // add"what can i do for you"
+        System.out.println("What can i do for you?");
+    }
+    //add"good bye "
+    public static void goodbye(){
+        System.out.println("Bye.Hope to see you again soon!");
+
     }
 }
