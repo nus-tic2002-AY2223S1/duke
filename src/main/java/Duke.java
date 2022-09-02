0294f29d482b727a0f392671c340221993c02ed8
DESKTOP-VFA5NMP\User <e0493677@u.nus.edu>
2022-09-02 20:30:36 +0800

    Level-1: Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,34 @@
+import java.util.Scanner;
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = "__           _        \n"
+                + "| |    _   _| | _____ \n"
+                + "| |   | | | | |/ / _ \\\n"
+                + "| |__ | |_| |   <  __/\n"
+                + "|____| \\__,_|_|\\_\\___|\n";
+        String line = "____________________________________________________________";
+
+        System.out.println(line);
+        System.out.println(logo);
+        System.out.println("Hello! I'm Luke");
+        System.out.println("What can I do for you?");
+        System.out.println(line);
+
+        while(true){
+            String input;
+
+            Scanner in = new Scanner(System.in);
+            input = in.nextLine();
+
+            System.out.println(line);
+            if (input.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                System.out.println(line);
+                break;
+            }
+            System.out.println(input);
+            System.out.println(line);
+        }
+
     }
 }
