9572ff139cce50942ea45e183d4d2383bf7bbcaf
Huyuanyuan <e0648923@u.nus.edu>
2022-09-02 17:45:02 +0800

    level1-1:Greet,Echo,Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,38 @@
+import java.util.Scanner;
+
 public class Duke {
+    public static String line = "\t____________________________________________________________\n";
+
     public static void main(String[] args) {
+
+        System.out.println();
+
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("yuanyuan testing");
+//        System.out.println("Hello from\n" + logo);
+//        System.out.println("yuanyuan testing");
+
+        String greeting = line + "\tHello! I'm Duke\n" +
+                "\tWhat can I do for you?\n"
+                +line;
+
+        System.out.println(greeting);
+
+        Scanner in = new Scanner(System.in);
+        String input;
+        input = in.nextLine();
+
+        while(input!=null){
+            if(input.equals("bye")){
+                System.out.println(line + "\t Bye. Hope to see you again soon!\n"+line);
+                break;
+            }
+            System.out.println(line + "\t"+input+"\n"+line);
+            input = in.nextLine();
+        }
+
     }
 }
