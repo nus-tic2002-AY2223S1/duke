7172da3b8770fab1a3e69473854dd5b4bd1772fa
Hidhid8 <91423640+Hidhid8@users.noreply.github.com>
2022-09-06 07:24:16 +0800

    Changed chatbot name/logo and added a hello and goodbye greeting

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,17 @@
 public class Duke {
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        String logo = " _______ ______ ___  ___ ______\n"
+                    + "|_______|  __  |   \\/   |  __  |\n"
+                    + "   | |  | |  | |  |\\/|  | |  | |\n"
+                    + "   | |  | |__| |  |  |  | |__| |\n"
+                    + "   |_|  |______|__|  |__|______|\n";
+        String line = "_____________________________________________";
+        System.out.println(logo);
+        System.out.println(line);
+        System.out.println("Hello! It's Tomo here.");
+        System.out.println("How can I help you?");
+        System.out.println(line);
+        System.out.println("Bye. Take care and see you again :)");
+        System.out.println(line);
     }
 }
