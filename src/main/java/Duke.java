35e2832a3045109740d3dba4f865ce176076efdd
Junhui Tang <tang_junhui_93@hotmail.com>
2022-09-05 23:22:50 +0800

    Level 1.Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,43 @@
-public class Duke {
-    public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
+import java.util.Scanner;
 
-        System.out.println("Hello from\n" + logo);
+
+public class Duke {
+
+    public static String print(String msg){
+        return "\t  ________________________________\n"+msg+"\t  ________________________________\n";
+
+
+    }
+
+
+    public static void main(String[] args) {
+        String greet ="\t\t        Hello! I'm \n"
+                +"\t\t   ____        _        \n"
+                + "\t\t  |  _ \\ _   _| | _____ \n"
+                + "\t\t  | | | | | | | |/ / _ \\\n"
+                + "\t\t  | |_| | |_| |   <  __/\n"
+                + "\t\t  |____/ \\__,_|_|\\_\\___|\n"
+                +"\t\t   What can I do for you?\n";
+
+        System.out.println(print(greet));
+        Scanner in= new Scanner(System.in);
+        String tempMsg;
+
+        while(true){
+            String userInput=in.nextLine();
+
+            if(userInput.equals("bye")){
+                tempMsg="\t  Bye. Hope to see you again soon!\n";
+                System.out.println(print(tempMsg));
+                break;
+            }
+            else{
+
+                System.out.println(print("\t  "+userInput+"\n"));
+            }
+
+
+
+        }
     }
 }
