3d1b6e8f3ca853f477bdf5493ce6d8353f6f2646
LiuYHYH <89028501+LiuYHYH@users.noreply.github.com>
2022-09-07 00:11:26 +0800

    Level 1-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,7 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +10,26 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        List<String> tasks = new ArrayList<>();
+        while (true){
+            String text;
+            Scanner s = new Scanner(System.in);
+            text = s.nextLine();
+            System.out.println("    ---------------------------------------");
+            System.out.println("    " + text);
+            if(text.equals("list")){
+                for (String task : tasks) {
+                    System.out.println("    " + task);
+                }
+            }
+            if(!text.equals("list") && !text.equals("bye")){
+                tasks.add(text);
+            }
+            if (text.equals("bye")) {
+                System.out.println("    Bye. Hope to see you again soon!");
+                break;
+            }
+        }
     }
 }
