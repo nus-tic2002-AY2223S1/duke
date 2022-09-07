178855775247bf10c6327284c5ad46a38d989133
James_2022 <jamesyuyanen@gmail.com>
2022-09-07 14:22:22 +0800

    edit level3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,40 +1,38 @@
 import java.util.Scanner;
 
 public class Duke {
+    private static int count = 0;
+    private static Task[] tasks = new Task[100];
+
+    public static void addList(Task t){
+        tasks[count]=t;
+        count++;
+    }
+    public static void print(String t){
+        if(t.equals("list")){
+                System.out.println("Here are the tasks in your list:");
+            for(int i=1;i<=count;i++){
+                System.out.println(i + "." + " " + tasks[i-1]);
+            }
+        }
+    }
 
     public static void main(String[] args) {
-       String line;
-         String[] book_list=new String[100];
+        String line;
         Scanner in = new Scanner(System.in);
-
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-        //
-       /*
-        while(true){
-            line=in.nextLine();
-            Echo t = new Echo(line);
-            if(line.equalsIgnoreCase("bye")){
-                t.get();
-                break;
-            }
-            t.get();
-        }
-        */
-        /*
-        while(true){
-            line=in.nextLine();
-            AddList t = new AddList(line);
-            if(line.equalsIgnoreCase("bye")){
-                t.get();
-                break;
-            }
-            t.get();
-        }
-        */
 
+        
+//        line = in.nextLine();
+//       // addList(new Task("read book"));
+//        Task t = new Task("read book");
+//        addList(t);
+//        t.markAsDone();
+//        addList(new Task("return book"));
+//        addList(new Task("buy bread"));
 
-
+        print("list");
 
     }
 }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,26 +1,24 @@
-public class Task extends AddList {
+public class Task {
+
       protected String description;
       protected boolean isDone;
 
       public Task(String description){
-          super(description);
-          this.description=description;
-          this.isDone=false;
+          this.description = description;
+          this.isDone = false;
       }
-
       public String getStatusIcon(){
+
           return (isDone ? "x":" "); //mark done task with X
       }
 
       public void markAsDone(){
+          this.isDone = true;
+      }
 
-           if(description.startsWith("mark")){
-
-               System.out.println("Nice! I've marked this task as done:");
-           }
-           if(description.startsWith("unmark")){
-               System.out.println("Ok, I've marked this task as not done yet:");
-           }
+      public String toString(){
+          return "[" + getStatusIcon() + "]" + " " + this.description;
+         // return "added" + " " + description;
       }
 
 }
