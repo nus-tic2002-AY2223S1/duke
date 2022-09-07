8d13be687d9e1c42a6bdc27cc06feeb048895393
bowen <xubowen495@gmail.com>
2022-09-07 16:45:56 +0800

    Level-2: Add, List

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.util.List;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -9,12 +11,12 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         String breakLine = "-------------------------------------------------";
         String tab = "     ";
-        System.out.println("Hello from\n" + logo + breakLine);
+        System.out.println("Hello from bowen's\n" + logo + breakLine);
         System.out.println(tab + breakLine);
         System.out.println(tab + "Hello! I'm Duke\n" + tab +"What can I do for you?");
         System.out.println(tab + breakLine);
 
-
+        List<String> tasks = new ArrayList<>();
         String text;
         whileLoop: while (true) {
             text = inputText();
@@ -22,8 +24,12 @@ public class Duke {
                 case "bye":
                     echoText("Bye. Hope to see you again soon!");
                     break whileLoop;
+                case "list":
+                    printList(tasks);
+                    break;
                 default:
-                    echoText(text);
+                    tasks.add(text);
+                    echoText("added: " + text);
             }
         }
 
@@ -39,5 +45,15 @@ public class Duke {
         System.out.println(tab + breakLine);
         System.out.println(tab + text);
         System.out.println(tab + breakLine);
+    }
+    public static void printList(List<String> tasks) {
+        String breakLine = "-------------------------------------------------";
+        String tab = "     ";
+        System.out.println(tab + breakLine);
+        int counter = 0;
+        for (String task : tasks)
+            System.out.println(tab + ++counter + ". " + task);
+        System.out.println(tab + breakLine);
+
     }
 }
