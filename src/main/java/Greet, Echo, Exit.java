1d4f0ce5538510d215ea523da6e23211f95be605
LumWenKang <lumlaoren@gmail.com>
2022-09-07 11:46:56 +0800

    [Level-1] added greet and exit

---
+++ src/main/java/Greet, Echo, Exit.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+class assistant{
+    //method
+    public void greet() {
+        System.out.println("Hello! I'm Duke. \nWhat can I do for you?");
+    }
+
+    public void exit(){
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+};
