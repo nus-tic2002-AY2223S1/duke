1eea39a8853fde3d89330d748754047ec0056541
Hidhid8 <91423640+Hidhid8@users.noreply.github.com>
2022-09-09 23:53:49 +0800

    store user input in array and display it as a list when requested.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -14,20 +14,37 @@ public class Duke {
                     + "   | |  | |__| |  |  |  | |__| |\n"
                     + "   |_|  |______|__|  |__|______|\n";
         String line = "_____________________________________________";
+        String[] list = new String[100];
+        int listCount = 0; //to count number of items added to checklist
         System.out.println(logo);
         printbox("Hello! It's Tomo here.\nHow can I help you?");
 
         while(true) {
-            String input;
             Scanner in = new Scanner(System.in);
-            input = in.nextLine();
+            String input = in.nextLine();
+
             if(input.equals("bye")) {
                 break;
             }
+            else if(input.equals("list")) {
+                String combineList = "Checklist: \n";
+                int i = 0;
+
+                //loop used to combine array into a single string for printing
+                while(i!=listCount) {
+                    i++;
+                    combineList = combineList + String.valueOf(i) + ") " + list[i-1] + "\n";
+                }
+                combineList = combineList + "-end of list-";
+                printbox(combineList);
+            }
             else {
-                printbox(input); //echo user input
+                list[listCount] = input;
+                listCount++;
+                printbox("Added '" + input + "' into the list." ); //echo user input
             }
         }
+
         printbox("Bye! See you and take care :)");
     }
 }
