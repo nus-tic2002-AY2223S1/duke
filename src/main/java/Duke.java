3773eb1057752cce2821d53e72d92c3227162df1
James <jamesyuyanen@gmail.com>
2022-09-11 20:37:07 +0800

    edit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -20,22 +20,12 @@ public class Duke {
         Scanner in = new Scanner(System.in);
         System.out.println("Hello! I'm Duke");
         System.out.println("What can I do for you?");
-
-
-//        line = in.nextLine();
-//       // addList(new Task("read book"));
-//        Task t = new Task("read book");
-//        addList(t);
-//        t.markAsDone();
-//        addList(new Task("return book"));
-//        addList(new Task("buy bread"));
-
-        //printList("list");
-
-
+        
         while(true){
             line = in.nextLine();
-            int indexNum = Integer.parseInt(line.substring(line.indexOf(" ")+1));
+           int index = line.indexOf(" ")+1;
+          int indexNum =Integer.parseInt(line.substring(index));
+            System.out.print(indexNum);
             switch(line){
                 case"bye":
                     System.out.println("Bye. Hope to see you again soon!");
@@ -44,12 +34,14 @@ public class Duke {
                     printList();
                     continue;
                 case"mark":
+
                     Task markTask = tasks[indexNum];
                     System.out.println("Nice! I've marked this task as done:");
                     markTask.markAsDone();
                     System.out.println(markTask);
                     continue;
                 case"unmark":
+
                     Task unmarkTask = tasks[indexNum];
                     System.out.println("OK, I've marked this task as not done yet:");
                     System.out.println(unmarkTask);
@@ -58,7 +50,6 @@ public class Duke {
                     addList(new Task(line));
             }
         }
-
     }
 }
 
