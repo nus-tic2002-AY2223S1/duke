1381d7da1b982015e7d1037fbb5faf17d47f9f76
DESKTOP-1CIU4U4\Aaron <e0493794@u.nus.edu>
2022-09-13 03:23:41 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,23 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        String hello = "Hello! I'm Rick\n"
+                + "What can I do for you?\n\n";
+        System.out.print(hello);
+
+        inputCommand();
+    }
+    public static void inputCommand() {
+        Scanner in = new Scanner(System.in);
+        String command = in.nextLine();
+
+        if (command.equalsIgnoreCase("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");
+        }
+        else {
+            System.out.println(command + System.lineSeparator());
+            inputCommand();
+        }
     }
 }
