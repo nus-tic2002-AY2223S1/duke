a39bf3375aa7f18c6b895b66e65bd77110f0136a
DESKTOP-1CIU4U4\Aaron <e0493794@u.nus.edu>
2022-09-13 17:19:11 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -15,35 +15,69 @@ public class Duke {
 
         String[] tasksArray = new String[100];
 
+       // Task t = new Task();
+
         inputCommand(tasksArray, 0);
-
-
     }
-    public static void inputCommand(String[] tasks, int count) {
+    public static void inputCommand(String[] tasksArr, int count) {
         Scanner in = new Scanner(System.in);
         String command = in.nextLine();
 
         if (command.equalsIgnoreCase("bye")) {
             System.out.println("Bye. Hope to see you again soon!");
         }
+
         else if (command.equalsIgnoreCase("list")) {
             int listNo = 0;
 
-            for (String element : tasks) {
+            System.out.println("Here are the tasks in your list:");
+
+            for (String element : tasksArr) {
                 listNo++;
                 if (element == null) {
                     System.out.println();
                     break;
                 } else
-                    System.out.println(listNo + ". " + element);
+                    System.out.println(listNo + "." + element);
             }
 
-            inputCommand(tasks, count);
+            inputCommand(tasksArr, count);
         }
+
+        else if (command.startsWith("mark")) {
+            int taskNo = Integer.parseInt(command.substring(5)) - 1;
+            String taskName = tasksArr[taskNo].substring(4);
+
+            Task t = new Task(taskName);
+            t.markAsDone(true);
+
+            tasksArr[taskNo] = "[" + t.getStatusIcon() + "] " + taskName;
+            System.out.println("Nice! I've marked this task as done:" + System.lineSeparator()
+                    + tasksArr[taskNo] + System.lineSeparator());
+
+            inputCommand(tasksArr, count);
+        }
+
+        else if (command.startsWith("unmark")) {
+            int taskNo = Integer.parseInt(command.substring(7)) - 1;
+            String taskName = tasksArr[taskNo].substring(4);
+
+            Task t = new Task(taskName);
+            t.markAsDone(false);
+
+            tasksArr[taskNo] = "[" + t.getStatusIcon() + "] " + taskName;
+            System.out.println("OK, I've marked this task as not done yet:" + System.lineSeparator()
+                    + tasksArr[taskNo] + System.lineSeparator());
+
+            inputCommand(tasksArr, count);
+        }
+
         else {
-            tasks[count] = command;
             System.out.println("added: " + command + System.lineSeparator());
-            inputCommand(tasks,count + 1);
+
+            tasksArr[count] = "[ ] " + command;
+
+            inputCommand(tasksArr,count + 1);
         }
     }
 }
