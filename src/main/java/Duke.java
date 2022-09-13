3ee713ea3ce0333afe119288a82187e0697aa11c
Jsleo <jsleo906@gmail.com>
2022-09-13 13:48:21 +0800

    Greet, Echo, Exit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,15 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+       
+        try (Scanner in = new Scanner(System.in)) {
+        System.out.println("\nHello! I'm Duke\nWhat can I do for you?\n");
+        String input = in.nextLine();
+        if (input.equals("bye")) {
+            System.out.println("Bye. Hope to see you again soon!");  
+            } else {
+            System.out.println(input);
+            }
+        }
     }
 }
