290cd7c5ec5af003d998eb7770915345f87903c9
siying_png <siying.png@hotmail.com>
2022-09-13 18:08:21 +0800

    Level-1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,5 +8,19 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        System.out.println("Hello! I'm Duke" + System.lineSeparator() + "What can I do for you?");
+
+        String userReply;
+        Scanner in = new Scanner(System.in);
+        userReply = in.nextLine();
+
+        while (!userReply.equals("bye")) {
+            System.out.println(userReply);
+            in = new Scanner(System.in);
+            userReply = in.nextLine();
+        }
+            System.out.println("Bye. Hope to see you again soon!");
+
     }
 }
