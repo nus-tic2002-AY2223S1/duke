1cc694dbbf31f2efb6c23603bca08e447c711b0d
Ayesha Begum <ayeshabegum02@gmail.com>
2022-09-17 18:06:56 +0800

    added level 4

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,10 +1,27 @@
 public class Item {
     private String name;
     private boolean isDone;
+    private String type;
 
-    public Item(String n){
-        this.name = n;
+    private String time;
+
+    public Item(String n, String t){
         this.isDone = false;
+        this.type = t;
+        switch (t){
+            case "T":
+                this.name = (n.replaceAll("todo","")).trim();
+                this.time = "";
+                break;
+            case "E":
+            case "D":
+                n = n.replaceAll("deadline","");
+                n = n.replaceAll("event","");
+                String[] f = formatPassedName(n);
+                this.name = f[0];
+                this.time = f[1];
+                break;
+        }
     }
 
     public String getName(){
@@ -19,5 +36,32 @@ public class Item {
     }
     public void setIsDone(boolean d){
         this.isDone = d;
+    }
+
+    public String getType(){
+        return this.type;
+    }
+    public void setType(String t){
+        this.type = t;
+    }
+
+    public String getTime(){
+        return this.time;
+    }
+    public void setTime(String t){
+        this.time = t;
+    }
+
+    private String[] formatPassedName(String n){
+        String[] formatted = new String[2];
+        Integer idx = n.indexOf("/");
+        formatted[0] = n.substring(0, idx-1).trim();
+        String x = n.substring(idx+1,idx+3);
+        if(x.equals("at") || x.equals("by")){
+            formatted[1] = n.substring(idx+3).trim();
+        } else {
+            formatted[1] = n.substring(idx+1).trim();
+        }
+        return formatted;
     }
 }
