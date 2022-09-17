60ad2fcad8843d482ac52897982507d4b8cade1c
Ayesha Begum <ayeshabegum02@gmail.com>
2022-09-17 14:50:08 +0800

    added level 0

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,10 +1,43 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class Duke {
     public static void main(String[] args) {
+        intro();
+    }
+
+    private static void intro(){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        System.out.format("Hello! I'm \n%s\nWhat can I do for you?\n", logo);
+        conversation();
+    }
+
+    private static void conversation(){
+        String inp = "";
+        Scanner scanner = new Scanner(System.in);
+
+        while(!isClosing(inp = scanner.next())) {
+            if(isOpening(inp)){
+                System.out.println("\tHello again!\n");
+            } else {
+                System.out.format("\t%s\n", inp);
+            }
+        }
+        System.out.println("\tBye. Hope to see you again soon!!");
+        System.exit(1);
+    }
+
+    private static boolean isOpening(String text){
+        String[] opening = {"Hello","Hi"};
+        return Arrays.stream(opening).anyMatch(text::equalsIgnoreCase);
+    }
+
+    private static boolean isClosing(String text){
+        String[] closing = {"Bye","See ya"};
+        return Arrays.stream(closing).anyMatch(text::equalsIgnoreCase);
     }
 }
