7f9090eeee5ea1cc6589ec541d089035e23342c2
Ayesha Begum <ayeshabegum02@gmail.com>
2022-09-17 17:13:17 +0800

    added level 3 and refactoring

--- src/main/java/Item.java
+++ src/main/java/Item.java
@@ -1,8 +1,10 @@
 public class Item {
     private String name;
+    private boolean isDone;
 
     public Item(String n){
         this.name = n;
+        this.isDone = false;
     }
 
     public String getName(){
@@ -10,5 +12,12 @@ public class Item {
     }
     public void setName(String n){
         this.name = n;
+    }
+
+    public boolean getIsDone(){
+        return this.isDone;
+    }
+    public void setIsDone(boolean d){
+        this.isDone = d;
     }
 }
--- src/main/java/Todo.java
+++ src/main/java/Todo.java
@@ -7,11 +7,38 @@ public class Todo {
         items = new ArrayList<>(100);
     }
 
+    private void printList(){
+        for(int i = 0; i < items.size(); i++){
+            printItem(i+1, items.get(i));
+        }
+    }
+
+    private void printItem(Integer n, Item i){
+        System.out.format("\t%d.[%s] %s\n", n, i.getIsDone() ? "X":" ", i.getName());
+    }
+
     public void addItem(String itemName){
         items.add(new Item(itemName));
     }
 
-    public ArrayList<Item> getItems(){
-        return this.items;
+    public void showList(){
+        System.out.println("\tHere are the tasks in your list:");
+        printList();
+    }
+
+    public void updateDoneState(int n, boolean isDone){
+        if(n > 0 && n <= items.size()) {
+            Item item = items.get(n-1);
+            item.setIsDone(isDone);
+            if (isDone) {
+                System.out.println("\tNice! I've marked this task as done:");
+            } else {
+                System.out.println("\tOK, I've marked this task as not done yet:");
+            }
+            printItem(n, item);
+        } else {
+            System.out.println("\tOops! I could not find this task. Here are the tasks again:");
+            printList();
+        }
     }
 }
