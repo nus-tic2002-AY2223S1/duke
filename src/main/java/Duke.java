4fabaa104340096c334979c31ac78554f067e0fd
LAPTOP-5MA0485T\carisse <theseiracarisse@gmail.com>
2022-09-21 19:54:13 +0800

    Add Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,28 +1,23 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static String line = "\n----------------------------------\n";
-    static private String userInput = "";
-    static private Task taskList[] = new Task[100];
-    static private int taskListCount = 0;
+    public static String line = "\n--------------------------------------------------------------------\n";
+    static public String userInput = "";
+    static public Task taskList[] = new Task[100];
+    static public int taskListCount = 0;
 
-    public static void list() {
+/*    public static void list() {
         System.out.println(line);
         for (int i = 0; i < taskListCount; i++) {
             System.out.println(i+1 + ". " + "[" + taskList[i].getStatusIcon() + "] " + taskList[i].description);
         }
         System.out.println(line);
-    }
-
-    public static void addList(Task task) {
-        taskList[taskListCount] = task;
-        taskListCount++;
-        System.out.println(line + "added: " + taskList[taskListCount-1].description + line);
-    }
+    }*/
 
     public static void bye() {
         System.out.println(line + "Bye. Hope to see you again soon!" + line);
     }
+
 
     public static void main(String[] args) {
         String logo = "⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠔⠋⠀⠐⠈⠉⠉⠉⠀⠀⠀⠉⠓⢄⠀⣀⠄⠚⠙⠉⠁⠀⠉⠉⠉⠁⠂⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n" +
@@ -58,27 +53,19 @@ public class Duke {
 
         userInput = scanner.nextLine();
         while(!userInput.equalsIgnoreCase("bye")) {
-            switch(userInput) {
-                case "list":
-                    list();
-                    break;
-                case "mark":
-                    System.out.println("Which task would you like to mark done?");
-                    String mark = scanner.nextLine();
-                    int userMark = Integer.parseInt(mark);
-                    taskList[userMark-1].markAsDone();
-                    break;
-                case "unmark":
-                    System.out.println("Which task would you like to mark as not done?");
-                    String unmark = scanner.nextLine();
-                    int userUnmark = Integer.parseInt(unmark);
-                    taskList[userUnmark-1].markAsUndone();
-                    break;
-                default:
-                    Task t = new Task(userInput);
-                    t.description = userInput;
-                    addList(new Task(userInput));
+            if (userInput.startsWith("list")) {
+                Task.list();
             }
+            else if(userInput.contains("mark")) {
+                Task.markTask();
+            }
+            else if (userInput.startsWith("todo") || userInput.startsWith("deadline") || userInput.startsWith("event")) {
+                Task.addTask(userInput);
+                System.out.println(line + "Got it. I've added this task:\n");
+                System.out.println(taskList[taskListCount-1].toString());
+                System.out.println("Now you have " + taskListCount + " tasks in the list." + line);
+            }
+
             userInput = scanner.nextLine();
         }
         bye();
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -1,17 +1,78 @@
 public class Task {
-    protected String description;
+    protected static String description = "";
     protected boolean isDone;
+    public  String[] split;
 
     public Task(String description) {
         this.description = description;
         this.isDone = false;
     }
 
+    public static void addTask (String input) throws RuntimeException {
+        String des = "";
+        String date = "";
+        int dateIdx = 0;
+        Duke.taskListCount++;
+
+        String[] split = Duke.userInput.split(" ");
+        for (int i=1; i<split.length; i++) {
+            if (split[i].startsWith("/")) {
+                dateIdx = i;
+                break;
+            }
+            des = des + split[i] + " ";
+        }
+
+        for (int j=dateIdx+1; j<split.length; j++) {
+            date = date + split[j] + " ";
+        }
+
+        switch (split[0]) {
+            case "todo":
+                Duke.taskList[Duke.taskListCount-1] = new ToDo(des);
+                break;
+            case "deadline":
+                Duke.taskList[Duke.taskListCount-1] = new Deadline(des, date);
+                break;
+            case "event":
+                Duke.taskList[Duke.taskListCount-1] = new Event(des, date);
+                break;
+            default:
+                Duke.taskList[Duke.taskListCount-1] = new Task(input);
+                break;
+        }
+    }
+
+    public static void list() {
+        System.out.println(Duke.line);
+        System.out.println("Here are the tasks in your list:\n");
+        for (int i = 0; i < Duke.taskListCount; i++) {
+            System.out.println(i+1 + ". " + Duke.taskList[i].toString());
+        }
+        System.out.println(Duke.line);
+    }
+
+    public static void markTask() {
+        try {
+            String[] split = Duke.userInput.split(" ");
+            int taskId = (Integer.parseInt(split[1])) - 1;
+            if (Duke.userInput.contains("un")) {
+                Duke.taskList[taskId].markAsNotdone();
+            }
+            else {
+                Duke.taskList[taskId].markAsDone();
+            }
+        }
+        catch (IndexOutOfBoundsException e) {
+            System.out.println("Something went wrong. Please enter a valid operation. E.g. mark 1");
+        }
+    }
+
     public String getTask() {
         return this.description;
     }
 
-    public void markAsUndone() { isDone = false;
+    public void markAsNotdone() { isDone = false;
         System.out.println(Duke.line + "OK, I've marked this task as not done yet.");
         System.out.println("[ ] " + description + Duke.line);
     }
@@ -24,5 +85,10 @@ public class Task {
 
     public String getStatusIcon() {
         return (isDone ? "X" : " ");
+    }
+
+    @Override
+    public String toString() {
+        return "[" + getStatusIcon() + "] " + description;
     }
 }
