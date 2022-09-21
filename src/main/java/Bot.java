9c477709e44396e5e94cbb12cf0e95435db89250
denny-wongso <denny.wongso@gmail.com>
2022-09-22 00:06:44 +0800

    - Used Task object instead of primitive string for storage - Added mark and unmark task

--- src/main/java/Bot.java
+++ src/main/java/Bot.java
@@ -2,13 +2,13 @@ import java.util.Arrays;
 public class Bot {
     private String name;
     private String seperator;
-    private String[] storage;
+    private Task[] storage;
     private int index;
 
     public Bot(String name, String seperator) {
         this.name = name;
         this.seperator = seperator;
-        this.storage = new String[100];
+        this.storage = new Task[100];
         this.index = 0;
     }
 
@@ -27,13 +27,40 @@ public class Bot {
     }
 
     public void showList() {
-
         int i = 1;
         seperator();
-        String[] stored = Arrays.copyOf(storage, index);
-        for(String each : stored) {
+        System.out.println(appendTab("Here are the tasks in your list:"));
+        Task[] stored = Arrays.copyOf(storage, index);
+        for(Task each : stored) {
             System.out.println(appendTab(i+". " + each));
             i++;
+        }
+        seperator();
+    }
+
+    public void mark(String i){
+        int index = Integer.parseInt(i);
+        seperator();
+        if(storage[index - 1].isDone() == false) {
+            storage[index - 1].markTask(true);
+            System.out.println(appendTab("Nice! I've marked this task as done:"));
+            System.out.println(appendTab(storage[index - 1].toString()));
+        } else {
+            System.out.println(appendTab("This task is already marked as done"));
+        }
+        seperator();
+
+    }
+
+    public void unmark(String i){
+        int index = Integer.parseInt(i);
+        seperator();
+        if(storage[index - 1].isDone() == true) {
+            storage[index - 1].markTask(false);
+            System.out.println(appendTab("OK! I've marked this task as not done yet:"));
+            System.out.println(appendTab(storage[index - 1].toString()));
+        } else {
+            System.out.println(appendTab("This task is already marked as not done"));
         }
         seperator();
     }
@@ -48,7 +75,8 @@ public class Bot {
         System.out.println("\t" + seperator);
     }
     private void store(String value) {
-        storage[index] = value;
+        Task newTask = new Task(value);
+        storage[index] = newTask;
         index++;
     }
 
