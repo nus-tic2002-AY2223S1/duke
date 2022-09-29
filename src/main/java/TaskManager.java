7b8744b7d2fdf98766ed05d6c446b0ee6b2d2af6
denny-wongso <denny.wongso@gmail.com>
2022-09-29 19:54:05 +0800

    added taskmanager to be the wrapper on all the taskinterface classes

---
+++ src/main/java/TaskManager.java
@@ -0,0 +1,77 @@
+import Tasks.*;
+import Tasks.TaskInterface;
+
+import java.util.ArrayList;
+
+public class TaskManager implements TaskInterface {
+    ArrayList<TaskInterface> tasks = new ArrayList<>();
+
+    public void addNewTask(TaskType type, String work) {
+        switch (type) {
+            case TODO:
+                Todo todo = new Todo(work);
+                tasks.add(todo);
+                break;
+            case EVENT:
+                Event event = new Event(work);
+                tasks.add(event);
+                break;
+            case DEADLINE:
+                Deadline deadline = new Deadline(work);
+                tasks.add(deadline);
+                break;
+        }
+    }
+    @Override
+    public String getString() {
+        return tasks.get(tasks.size() - 1).getString();
+    }
+
+    public ArrayList<String> getList() {
+        ArrayList<String> list = new ArrayList<>();
+        int i = 1;
+        for(TaskInterface task: tasks) {
+            list.add(task.getString());
+        }
+        return list;
+    }
+
+    public boolean isDone() {
+        return tasks.get(0).isDone();
+    }
+
+
+    public void markTask(boolean mark){
+        tasks.get(0).markTask(mark);
+    }
+
+    public boolean markTask(int index, boolean mark) {
+        if(tasks.get(index).isDone() == mark) {
+            return false;
+        } else {
+            tasks.get(index).markTask(mark);
+            return true;
+        }
+    }
+
+    public String getTask(int index) {
+        return tasks.get(index).getString();
+    }
+
+    public enum TaskType {
+        DEADLINE, EVENT, TODO;
+
+        public static TaskType getType(String type) throws UnsupportedTaskType {
+            if(type.equalsIgnoreCase("deadline")) {
+                return DEADLINE;
+            } else if (type.equalsIgnoreCase("event")) {
+                return EVENT;
+            } else if (type.equalsIgnoreCase("todo")) {
+                return TODO;
+            } else {
+                throw new UnsupportedTaskType();
+            }
+        }
+    }
+}
+
