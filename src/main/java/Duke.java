dd4940f642525e2a30c97fdef1c819e77926d6cd
Junhui Tang <tang_junhui_93@hotmail.com>
2022-10-05 22:47:35 +0800

    Level-5. Handle Errors

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,97 +2,89 @@ import java.util.Scanner;
 
 
 public class Duke {
-
-    public static String print(String msg){
-        return "________________________________\n"+msg+"________________________________\n";
-
-
+    public static Task task[]=new Task[100];
+    public static  int countTask=0;
+    public static final String byeMsg="Bye. Hope to see you again soon!\n";
+    public static String print(String msg) {
+        return "________________________________\n" + msg + "________________________________\n";
     }
 
 
     public static void main(String[] args) {
-        String greet ="Hello! I'm Duke\nWhat can I do for you?\n";
+        String greet = "Hello! I'm Duke\nWhat can I do for you?\n";
 
         System.out.println(print(greet));
-        Scanner in= new Scanner(System.in);
-        String tempMsg;
-        Task[] task=new Task[100];
+        Scanner in = new Scanner(System.in);
 
-        int countTask=0;
-        String itemList="";
-	int indexS;
-	int index;
-	String detailTask;
-	String detail;	
 
-        while(true) {
+
+
+
+
+        while (true) {
             String userInput = in.nextLine();
-            String[] sentence=userInput.split(" ");
-            int itemNo=999;
-            
+            String[] inputSplit = userInput.split(" ");
+            int itemNo;
 
-            switch (sentence[0]) {
+
+            switch (inputSplit[0]) {
                 case "bye":
-                    tempMsg = "Bye. Hope to see you again soon!\n";
-                    System.out.println(print(tempMsg));
+                    System.out.println(print(byeMsg));
                     return;
 
-            case "list":
-                for (int i = 0; i < countTask; i++) {
-                    //itemList += "(i + 1) + ".["+task[i].getStatusIcon()+"] " + task[i].getTask() + "\n";
-		    itemList +=(i+1)+"."+task[i].toString();
-                }
-                System.out.println(print(itemList));
-                itemList = "";
-                break;
+                case "list":
+                    Task.list();
+                    break;
 
-            case "mark":
-		itemNo=Integer.parseInt(sentence[1])-1;
-                task[itemNo].mark();
-                System.out.println(print("Nice! I've marked this task as done:\n ["+task[itemNo].getStatusIcon()+"] "+task[itemNo].getTask()+"\n"));
-                break;
+                case "mark":
+                case "unmark":
 
-            case "unmark":
-		itemNo=Integer.parseInt(sentence[1])-1;
-                task[itemNo].unmark();
-                System.out.println(print("OK, I've marked this task as not done yet:\n ["+task[itemNo].getStatusIcon()+"] "+task[itemNo].getTask()+"\n"));
-                break;
+                    itemNo = Integer.parseInt(inputSplit[1]) - 1;
+                    try{
+                        if(itemNo>=countTask || itemNo<0){
+                            throw new DukeException();
+                        }
+                    }catch(DukeException e){
+                        System.out.println(print("☹ OOPS!!! Task ID not found, please check!\n"));
+                        break;
+                    }
+                    if(inputSplit[0].equals("mark")){
+                        task[itemNo].mark();
+                    }else {
+                        task[itemNo].unmark();
+                    }
+                    break;
 
-	    case "deadline":
-		
-		index=userInput.indexOf('/');
-		indexS=userInput.indexOf(' ');
-		detail=userInput.substring(index+4);
-		detailTask=userInput.substring(indexS,index-1);
-		task[countTask]=new Deadline(detailTask,detail);		
-	
-		System.out.println(print(task[countTask].encapTask(task[countTask].toString(),countTask+1)));
-		countTask++;
-		break;
-	    
-            case "todo":
-		indexS=userInput.indexOf(' ');
-		detailTask=userInput.substring(indexS);
-		task[countTask]=new Todo(detailTask);
-		
-		System.out.println(print(task[countTask].encapTask(task[countTask].toString(),countTask+1)));
-		countTask++;
-		break;
 
-	    case "event":
-		
-		index=userInput.indexOf('/');
-		indexS=userInput.indexOf(' ');
-		detail=userInput.substring(index+4);
-		detailTask=userInput.substring(indexS,index-1);
-		task[countTask]=new Event(detailTask,detail);		
-	
-		System.out.println(print(task[countTask].encapTask(task[countTask].toString(),countTask+1)));
-		countTask++;
-		break;	
-	
-            default:
-                break;
+                case "deadline":
+                case "todo":
+                case "event":
+                    try{
+                        if(inputSplit.length==1){
+                            throw new DukeException();
+                        }
+                    }catch(DukeException e){
+
+                        System.out.println(print("☹ OOPS!!! The description of a "+inputSplit[0] +" cannot be empty.\n"));
+                        break;
+                    }
+
+                    try {
+                        Task.addTask(inputSplit[0], userInput);
+                    }catch(DukeException e){
+                        System.out.println(print("☹ OOPS!!! Invalid input, please check!\n"));
+                    }
+                    countTask++;
+                    break;
+
+                default:
+                    try{
+                        throw new DukeException();
+                    }catch(DukeException e){
+                        System.out.println(print("☹ OOPS!!! I'm sorry, but I don't know what that means :-(\n"));
+
+                    }
+                    break;
 
 
             }
--- src/main/java/Task.java
+++ src/main/java/Task.java
@@ -16,11 +16,13 @@ public class Task {
         return description;
     }
 
-    public boolean mark(){
-        return isDone=true;
+    public void mark(){
+        isDone=true;
+        System.out.println(Duke.print("Nice! I've marked this task as done:\n [" + getStatusIcon() + "] " + getTask() + "\n"));
     }
-    public boolean unmark(){
-        return isDone=false;
+    public void unmark(){
+        isDone=false;
+        System.out.println(Duke.print("OK, I've marked this task as not done yet:\n [" + getStatusIcon() + "] " + getTask() + "\n"));
     }
     public String toString(){
 	return ("["+getStatusIcon()+"] "+description);	
@@ -28,4 +30,59 @@ public class Task {
     public String encapTask(String t, int count){
 	return ("Got it. I've added this task:\n "+ t +"Now you have "+count+" tasks in the list.\n");
     }
+
+    public static void list(){
+        String itemList="";
+        for (int i = 0; i < Duke.countTask; i++) {
+            //itemList += "(i + 1) + ".["+task[i].getStatusIcon()+"] " + task[i].getTask() + "\n";
+            itemList += (i + 1) + "." + Duke.task[i].toString();
+
+        }
+        System.out.println(Duke.print(itemList));
+        itemList="";
+    }
+
+    public static void addTask(String taskType,String fullInput) throws DukeException{
+        int indexSlash= fullInput.indexOf('/');
+        int indexSpace= fullInput.indexOf(' ');
+        String detail;
+        String due;
+
+        if(taskType.equals("deadline")){
+            if(indexSlash==-1 || indexSpace==-1){
+                throw new DukeException();
+            }
+            detail=fullInput.substring(indexSpace, indexSlash - 1);
+            due=fullInput.substring(indexSlash + 4);
+
+            Duke.task[Duke.countTask] = new Deadline(detail, due);
+            System.out.println(Duke.print(Duke.task[Duke.countTask].encapTask(Duke.task[Duke.countTask].toString(), Duke.countTask + 1)));
+
+        }
+        else if(taskType.equals("todo")){
+            if(indexSpace==-1){
+                throw new DukeException();
+            }
+            detail = fullInput.substring(indexSpace);
+
+            Duke.task[Duke.countTask] = new Todo(detail);
+            System.out.println(Duke.print(Duke.task[Duke.countTask].encapTask(Duke.task[Duke.countTask].toString(), Duke.countTask + 1)));
+
+        }
+        else{
+            if(indexSlash==-1 || indexSpace==-1){
+                throw new DukeException();
+            }
+            detail=fullInput.substring(indexSpace, indexSlash - 1);
+            due=fullInput.substring(indexSlash + 4);
+
+            Duke.task[Duke.countTask] = new Event(detail, due);
+            System.out.println(Duke.print(Duke.task[Duke.countTask].encapTask(Duke.task[Duke.countTask].toString(), Duke.countTask + 1)));
+
+        }
+
+    }
+
+
+
 }
