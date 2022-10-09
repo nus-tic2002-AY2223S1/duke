f2196d48bb569c8f81562b23c655fbfd4a0716e6
rajeevan <rajeevanss@spdigital.sg>
2022-10-10 02:30:17 +0800

    Level-4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -7,6 +7,10 @@ public class Duke {
     public static final String LIST_COMMAND  ="list";
     public static final String MARK_COMMAND  ="mark";
     public static final String UNMARK_COMMAND  ="unmark";
+    public static final String TODO_COMMAND ="todo";
+    public static final String DEADLINE_COMMAND  ="deadline";
+    public static final String EVENT_COMMAND  ="event";
+
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         printBanner();
@@ -15,9 +19,7 @@ public class Duke {
     }
 
     private static void repLoop(Scanner scanner){
-        String existCue;
         List<Task> inputList = new ArrayList<Task>(100);
-
         String input ="";
         input = scanner.nextLine();
         while(!input.equals(EXIT_COMMAND)){
@@ -28,6 +30,7 @@ public class Duke {
     }
     private static void executeCommand(String command,List<Task> tasks){
         String[] inputs = command.split(" ");
+        String[] commandArgs;
         switch(inputs[0]){
             case LIST_COMMAND :
                 listTasks(tasks);
@@ -40,11 +43,34 @@ public class Duke {
                 break;
             case EXIT_COMMAND:
                 break;
+            case TODO_COMMAND:
+                commandArgs = splitCommandArgs(command);
+                addTodo(tasks,commandArgs[0]);
+                break;
+            case DEADLINE_COMMAND:
+                commandArgs = splitCommandArgs(command);
+                addDeadline(tasks,commandArgs[0],commandArgs[1]);
+                break;
+            case EVENT_COMMAND:
+                commandArgs = splitCommandArgs(command);
+                addEvent(tasks,commandArgs[0],commandArgs[1]);
+                break;
             default:
-                addTask(tasks,command);
+                //previously default just adds whatever string input.
+                // Now it has to be Deadline ,Event or todoo:0.
+                System.out.println("");
                 break;
 
         }
+    }
+
+    //Splits the input from commandline for Deadlines and Events to the desc and time components
+    private static String[] splitCommandArgs(String input){
+        String commandArgs[];
+        int indexOfFirstSpace= (input.split(" ")[0].length());
+        // use this regex for simplicity as a deadline is going to use /by or /at
+        commandArgs = input.substring(indexOfFirstSpace).split("/[a-z]{2}");
+        return commandArgs;
     }
 
     private static void listTasks(List<Task> tasks){
@@ -55,12 +81,29 @@ public class Duke {
         }
         printDivider();
     }
+    @Deprecated
+    private static void addTask(List<Task> tasks,String desc){
+        tasks.add(new Task(desc));
+        printDivider();
+        System.out.println("\tadded: "+desc);
+        printDivider();
+    }
+    private static void addTodo(List<Task> tasks,String desc){
+        Todo t = new Todo(desc);
+        tasks.add(t);
+        printTaskAddAcknowledge(t, tasks.size());
+    }
 
-    private static void addTask(List<Task> tasks,String command){
-        tasks.add(new Task(command));
-        printDivider();
-        System.out.println("\tadded: "+command);
-        printDivider();
+    private static void addEvent(List<Task> tasks,String desc,String time){
+        Event e = new Event(desc,time);
+        tasks.add(e);
+        printTaskAddAcknowledge(e, tasks.size());
+    }
+
+    private static void addDeadline(List<Task> tasks,String desc,String time){
+        Deadline d = new Deadline(desc,time);
+        tasks.add(d);
+        printTaskAddAcknowledge(d, tasks.size());
     }
 
     private static void mark(List<Task> tasks,String taskIndex){
@@ -79,8 +122,16 @@ public class Duke {
         System.out.println("\t"+task.toString());
         printDivider();
     }
-
-
+    private static void printTaskAddAcknowledge(Task t,int count){
+        printDivider();
+        System.out.println("\tGot it. I've added this task:");
+        System.out.println("\t\t"+t.toString());
+        printTaskCount(count);
+        printDivider();
+    }
+    private static void printTaskCount(int count){
+        System.out.println("\tNow you have " + count+ " tasks in the list.");
+    }
     private static void printDivider(){
         System.out.println("\t____________________________________________________________");
     }
