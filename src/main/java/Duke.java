eb86a297e266ab2173ddae7b95450ba2cd79eeff
rajeevan <rajeevanss@spdigital.sg>
2022-10-09 22:10:38 +0800

    Level-3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 public class Duke {
     public static final String EXIT_COMMAND  ="bye";
     public static final String LIST_COMMAND  ="list";
+    public static final String MARK_COMMAND  ="mark";
+    public static final String UNMARK_COMMAND  ="unmark";
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         printBanner();
@@ -14,7 +16,7 @@ public class Duke {
 
     private static void repLoop(Scanner scanner){
         String existCue;
-        List<String> inputList = new ArrayList<String>(100);
+        List<Task> inputList = new ArrayList<Task>(100);
 
         String input ="";
         input = scanner.nextLine();
@@ -24,19 +26,58 @@ public class Duke {
         }
 
     }
-    private static void executeCommand(String command,List<String> inputs){
-        if (command.equals(LIST_COMMAND)){
-            printDivider();
-            for (int i = 0; i < inputs.size(); i++) {
-                System.out.println("\t"+ (i + 1)+". "+inputs.get(i));
-            }
-            printDivider();
-        }else{
-            inputs.add(command);
-            printDivider();
-            System.out.println("\tadded: "+command);
-            printDivider();
+    private static void executeCommand(String command,List<Task> tasks){
+        String[] inputs = command.split(" ");
+        switch(inputs[0]){
+            case LIST_COMMAND :
+                listTasks(tasks);
+                break;
+            case MARK_COMMAND:
+                mark(tasks,inputs[1]);
+                break;
+            case UNMARK_COMMAND:
+                unmark(tasks,inputs[1]);
+                break;
+            case EXIT_COMMAND:
+                break;
+            default:
+                addTask(tasks,command);
+                break;
+
         }
+    }
+
+    private static void listTasks(List<Task> tasks){
+        printDivider();
+        System.out.println("\tHere are the tasks in your list:");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println("\t"+ (i + 1)+". "+tasks.get(i).toString());
+        }
+        printDivider();
+    }
+
+    private static void addTask(List<Task> tasks,String command){
+        tasks.add(new Task(command));
+        printDivider();
+        System.out.println("\tadded: "+command);
+        printDivider();
+    }
+
+    private static void mark(List<Task> tasks,String taskIndex){
+        Task task = tasks.get(Integer.parseInt(taskIndex) - 1);
+        printDivider();
+        System.out.println("\tNice! I've marked this task as done:");
+        task.markAsDone();
+        System.out.println("\t"+task.toString());
+        printDivider();
+    }
+    private static void unmark(List<Task> tasks,String taskIndex){
+        Task task = tasks.get(Integer.parseInt(taskIndex) - 1);
+        printDivider();
+        System.out.println("\tOK, I've marked this task as not done yet:");
+        task.markAsNotDone();
+        System.out.println("\t"+task.toString());
+        printDivider();
     }
 
 
