1a15db58dab717254cb17b65ba16b848e7dec09e
becs <angpohye@gmail.com>
2022-10-11 20:48:21 +0800

    WIP

--- src/Duke.java
+++ src/Duke.java
@@ -3,69 +3,65 @@ import java.util.ArrayList;
 
 public class Duke {
     private static ArrayList <Task> wishList = new ArrayList<Task>();
+    private static int totalTasks = 0;
 
-    public static String input(){
-        Scanner s = new Scanner(System.in);
+    public static String input(Scanner s){
         String wish = s.nextLine();
         return wish;
+    }
+
+    public static void awakeDobby(){
+        System.out.println("My name is Dobby and I am a free elf!");
+        System.out.println("____________________Command Menu____________________");
+        System.out.println("mark | unmark | remove | view | bye | <<to do task>> | <<deadline>>/by (deadline)| <<event>>/at");
+        System.out.println("Automatic Testing: /bin/bash runtest.sh");
+        System.out.println("_____________________________________________________");
     }
 
     public static void bye(){
         System.out.print("Goodbye, my friend. Dobby must now hurry to Harry Potter.\n");
     }
 
-    public static Boolean isBye(String word){
-        if (word.equals("bye") || word.equals("Bye") ||
-                word.equals("goodbye") || word.equals("Goodbye") ||
-                word.equals("see you") || word.equals("See you") ||
-                word.equals("adios") || word.equals("Adios") ||
-                word.equals("ciao") || word.equals("Ciao") ||
-                word.equals("cya") || word.equals("Cya")){
-            return true;
-        } else {
-            return false;
-        }
-    }
-
     public static void storeWish(String wish){
         Task t;
         if (wish.contains("/by")){
-            t = new Deadline(wish); //Task t = new Task("read book");
+            t = new Deadline(wish);
         } else if (wish.contains("/at")){
-            t = new Event(wish); //Task t = new Task("read book");
+            t = new Event(wish);
         } else {
-            t = new Todo(wish); //Task t = new Task("read book");
+            t = new Todo(wish);
         }
         wishList.add(t);
-        System.out.print("Dobby has taken note of your wish: " + wish + ". ");
-        printWishList();
+        totalTasks = totalTasks + 1;
+        System.out.println("wish added: " + wish + ". ");
+        System.out.println("Now you have " + totalTasks + " tasks in the list.");
     }
 
     public static void markWish(String wish){ //mark as done
         int wNumber = Integer.parseInt(wish);
         (wishList.get(wNumber-1)).markAsDone();
-        System.out.print("Dobby has marked your wish as done");
-        printWishList();
+        System.out.println("Dobby has marked your wish as done");
+        System.out.println();
     }
 
     public static void unmarkWish(String wish){ //mark as done
         int wNumber = Integer.parseInt(wish);
         (wishList.get(wNumber-1)).markAsNotDone();
-        System.out.print("Dobby has marked your wish as done");
-        printWishList();
+        System.out.println("Dobby has unmarked your wish");
+        System.out.println();
     }
 
     public static void removeWish(String wish){
-        //int idx = wishList.indexOf(wish);
-        //wishList.remove(idx); // Remove wish that is currently stored at this index
         int wNumber = Integer.parseInt(wish);
         wishList.remove(wNumber-1);
-        System.out.print("Dobby has struck off " + wish + " from your wish list. ");
-        printWishList();
+        totalTasks = totalTasks - 1;
+        System.out.println("wish removed: " + wish);
+        System.out.println("Now you have " + totalTasks + " tasks in the list.");
+        System.out.println();
     }
 
     public static void printWishList(){
-        System.out.println("Here's your wish list that Dobby kept as a secret:");
+        System.out.println(" ");
         for (int i = 0; i < wishList.size(); i++) {
             System.out.println( "[" + wishList.get(i).getTaskType() + "][" + wishList.get(i).getStatusIcon() + "] " + wishList.get(i).getTask());
         }
@@ -77,24 +73,24 @@ public class Duke {
         return w;
     }
 
-    public static Boolean echo(){
-        String wish = input();
-        if (isBye(wish)){
+    public static boolean echo(Scanner s){
+        String wish = input(s);
+        if (wish.equals("bye")){
             bye();
             return true;
-        } else if (wish.contains("remove")){ // e.g. remove wish
+        } else if (wish.contains("remove") || wish.contains("Remove")){
             String w = substringHelper(wish);
             removeWish(w);
             return false;
-        } else if (wish.contains("unmark")) {
+        } else if (wish.contains("unmark") || wish.contains("Unmark")) {
             String w = substringHelper(wish);
             unmarkWish(w);
             return false;
-        }else if (wish.contains("mark")){ // e.g. i am done returning the book
+        }else if (wish.contains("mark") || wish.contains("Mark")){
             String w = substringHelper(wish);
             markWish(w);
             return false;
-        }else if(wish.equals("secret wish list")){ // What's my secret wish list?
+        }else if(wish.equals("wishlist")){
             printWishList();
             return false;
         }else{
@@ -104,13 +100,16 @@ public class Duke {
     }
 
     public static void main(String[] args) {
-        System.out.print("My name is Dobby and I am a free elf! Now, what is it that you wish for?");
 
-        Boolean terminateDobby = false;
+        awakeDobby();
+
+        Scanner s = new Scanner(System.in);
+        boolean terminateDobby = false;
         do{
-            terminateDobby = echo();
+            terminateDobby = echo(s);
             if (terminateDobby == false) {
-                System.out.print("Is there anything else?");
+                //System.out.println("Is there anything else?");
+                System.out.println("____________________");
             }
         }while(terminateDobby != true);
 
