4883b22b6906cf5fcc0f4fcd86309ffc55f3e2f0
xu kaipeng <>
2022-10-11 21:52:10 +0800

    add Deadline

---
+++ src/main/java/duke/task/Deadline.java
@@ -0,0 +1,15 @@
+package duke.task;
+
+public class Deadline extends Task{
+    protected  String by;
+    public Deadline(String description,String by){
+        super(description);
+        this.symbol = "D";
+        this.by = by;
+    }
+    
+    @Override
+    public String toString(){
+        return super.toString() + String.format("(by: %s)",this.by);
+    }
+}
