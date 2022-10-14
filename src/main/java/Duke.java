1a6ea8bb19e66a6aa0a21cb0da000a8eb2570327
lihui <lihui.pineapple@gmail.com>
2022-10-14 18:38:25 +0800

    Duke.java: added delete task

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -25,10 +25,9 @@ public class Duke {
         try {
             Task markedTask = taskList.get(markedIndex - 1);
             markedTask.markAsDone();
-        }
-        catch (IndexOutOfBoundsException e){
+        } catch (IndexOutOfBoundsException e) {
             System.out.println("____________________________________________________________" +
-                    "Task Number out of range. Please enter a valid input (Task Number: 1 " + "-" + taskList.size()+")"+
+                    "Task Number out of range. Please enter a valid input (Task Number: 1 " + "-" + taskList.size() + ")" +
                     "____________________________________________________________");
         }
     }
@@ -38,10 +37,9 @@ public class Duke {
         try {
             Task unmarkedTask = taskList.get(unmarkedIndex - 1);
             unmarkedTask.markAsUndone();
-        }
-        catch (IndexOutOfBoundsException e){
+        } catch (IndexOutOfBoundsException e) {
             System.out.println("____________________________________________________________" +
-                    "Task Number out of range. Please enter a valid input (Current Range: 1 " + "to " + taskList.size()+")"+
+                    "Task Number out of range. Please enter a valid input (Current Range: 1 " + "to " + taskList.size() + ")" +
                     "____________________________________________________________");
         }
     }
@@ -96,6 +94,22 @@ public class Duke {
         System.out.println("____________________________________________________________");
     }
 
+    public static void deleteTask(String[] lineSpaceSplit) {
+        System.out.println("____________________________________________________________");
+        int deleteIndex = Integer.parseInt(lineSpaceSplit[1]);
+        try {
+            Task task = taskList.get(deleteIndex - 1);
+            taskList.remove(task);
+            System.out.println("Noted. I've removed this task:\n");
+            System.out.println(task);
+            System.out.println("\nNow you have " + taskList.size() + " tasks in the list.");
+        } catch (IndexOutOfBoundsException e) {
+            System.out.println("____________________________________________________________" +
+                    "Task Number out of range. Please enter a valid input (Current Range: 1 " + "to " + taskList.size() + ")" +
+                    "____________________________________________________________");
+        }
+    }
+
     public static void main(String[] args) throws DukeException {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -112,74 +126,91 @@ public class Duke {
         while (true) {
             line = in.nextLine();
             String[] lineSpaceSplit = line.split(" ");
+            String taskTitle = lineSpaceSplit[0];
 
-            // enter bye to end chat
-            if (line.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!");
-                break;
-            }
+            switch (taskTitle) {
+                // enter bye to end chat
+                case ("bye"):
+                    System.out.println("Bye. Hope to see you again soon!");
+                    break;
 
-            // to list all items
-            else if (line.equals("list")) {
-                listTask();
-            }
+                // to list all items
+                case ("list"):
+                    listTask();
+                    break;
 
-            // mark items
-            else if (line.startsWith("mark")) {
-                if (taskList.isEmpty()){
-                    System.out.println("____________________________________________________________\n" +
-                            "No Task is found. Please create a task." +
-                            "\n____________________________________________________________");
-                }
-                else {
-                    try {
-                        markTask(lineSpaceSplit);
-                    } catch (ArrayIndexOutOfBoundsException e) {
+                // mark items
+                case ("mark"):
+                    if (taskList.isEmpty()) {
                         System.out.println("____________________________________________________________\n" +
-                                "Task Number not detected. Please enter a valid input (E.g. mark 1)" +
+                                "No Task is found. Please create a task." +
                                 "\n____________________________________________________________");
+                    } else {
+                        try {
+                            markTask(lineSpaceSplit);
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            System.out.println("____________________________________________________________\n" +
+                                    "Task number not detected. Please enter a valid input (E.g. mark 1)" +
+                                    "\n____________________________________________________________");
+                        }
                     }
-                }
-            }
+                    break;
 
-            // unmarked items
-            else if (line.startsWith("unmark")) {
-                if (taskList.isEmpty()){
-                    System.out.println("____________________________________________________________\n" +
-                            "No Task is found. Please create a task." +
-                            "\n____________________________________________________________");
-                }
-                else {
-                    try {
-                        unmarkTask(lineSpaceSplit);
-                    } catch (ArrayIndexOutOfBoundsException e) {
+                // unmarked items
+                case ("unmark"):
+                    if (taskList.isEmpty()) {
                         System.out.println("____________________________________________________________\n" +
-                                "No Task Number detected. Please enter a valid input (E.g. unmark 1)" +
+                                "No Task is found. Please create a task." +
                                 "\n____________________________________________________________");
+                    } else {
+                        try {
+                            unmarkTask(lineSpaceSplit);
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            System.out.println("____________________________________________________________\n" +
+                                    "Task number not detected. Please enter a valid input (E.g. unmark 1)" +
+                                    "\n____________________________________________________________");
+                        }
                     }
-                }
-            }
+                    break;
 
-            // to do task
-            else if (line.startsWith("todo")) {
-                todoTask(line);
-            }
+                // to do task
+                case ("todo"):
+                    todoTask(line);
+                    break;
 
-            // deadline task
-            else if (line.startsWith("deadline")) {
-                deadlineTask(line);
-            }
+                // deadline task
+                case ("deadline"):
+                    deadlineTask(line);
+                    break;
 
-            // event task
-            else if (line.startsWith("event")) {
-                eventTask(line);
-            }
+                // event task
+                case ("event"):
+                    eventTask(line);
+                    break;
 
-            // prompt user to enter valid input
-            else {
-                System.out.println("____________________________________________________________\n" +
-                        "Please enter a valid input (E.g. todo Task 1)" + "" +
-                        "\n____________________________________________________________");
+                // delete task
+                case ("delete"):
+                    if (taskList.isEmpty()) {
+                        System.out.println("____________________________________________________________\n" +
+                                "No Task is found. Please create a task." +
+                                "\n____________________________________________________________");
+                    } else {
+                        try {
+                            deleteTask(lineSpaceSplit);
+                        } catch (ArrayIndexOutOfBoundsException e) {
+                            System.out.println("____________________________________________________________\n" +
+                                    "Task number not detected. Please enter a valid input (E.g. delete 1)" +
+                                    "\n____________________________________________________________");
+                        }
+                    }
+                    break;
+
+                // prompt user to enter valid input
+                default:
+                    System.out.println("____________________________________________________________\n" +
+                            "Please enter a valid input (E.g. todo Task 1)" + "" +
+                            "\n____________________________________________________________");
+                    break;
             }
         }
     }
