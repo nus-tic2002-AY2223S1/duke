cd9ddaf5a745c4c8a30b8ad901e79ad797dd410a
Nur Hidayah <nurhidayah_halim@hotmail.com>
2022-10-17 22:45:03 +0800

    Error handling added

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,22 +1,10 @@
 import java.util.Scanner;
 
 public class Duke {
-    public static void printbox(String words) {
+    public static void printBox(String words) {
         System.out.println("_____________________________________________");
         System.out.println(words);
         System.out.println("_____________________________________________");
-    }
-
-    public static void printbox(String[] list, int count) {
-        String combineList = "Checklist: \n";
-        int i = 0;
-
-        while(i!=count) {
-            i++;
-            combineList = combineList + String.valueOf(i) + ") " + list[i-1] + "\n";
-        }
-        combineList = combineList + "-end of list-";
-        printbox(combineList);
     }
 
     public static void main(String[] args) {
@@ -25,39 +13,65 @@ public class Duke {
                     + "   | |  | |  | |  |\\/|  | |  | |\n"
                     + "   | |  | |__| |  |  |  | |__| |\n"
                     + "   |_|  |______|__|  |__|______|\n";
-        String line = "_____________________________________________";
-        String[] list = new String[100];
+        Task[] list = new Task[100];
         int listCount = 0; //to count number of items added to checklist
         System.out.println(logo);
-        printbox("Hello! It's Tomo here.\nHow can I help you?");
+        printBox("Hello! Tomo here.\nWhat's up? ^_^");
 
         while(true) {
             Scanner in = new Scanner(System.in);
             String input = in.nextLine();
-
-            if(input.equals("bye")) {
-                break;
-            }
-            else if(input.equals("list")) {
-                printbox(list, listCount);
-            }
-            else if(input.startsWith("mark")) {
-                int i = Character.getNumericValue(input.charAt(5));
-                list[i-1] = list[i-1].replace("[ ]", "[X]");
-                printbox(list, listCount);
-            }
-            else if(input.startsWith("unmark")) {
-                int i = Character.getNumericValue(input.charAt(7));
-                list[i-1] = list[i-1].replace("[X]", "[ ]");
-                printbox(list, listCount);
-            }
-            else {
-                list[listCount] = "[ ] " + input;
-                listCount++;
-                printbox("Added '" + input + "' into the list." ); //echo user input
+            String[] arrInput = input.split(" ", 2);
+            try {
+                if (arrInput[0].equals("bye")) {
+                    break;
+                } else if (arrInput[0].equals("list")) {
+                    String toPrint = "Checklist:";
+                    for (int i = 0; i < listCount; i++) {
+                        toPrint = toPrint + "\n" + (i + 1) + ") " + list[i].getTask();
+                    }
+                    printBox(toPrint);
+                } else if (arrInput[0].equals("mark")) {
+                    int i = Integer.parseInt(arrInput[1]);
+                    list[i - 1].setStatus(true);
+                    printBox("Task completed: " + list[i - 1].getTask());
+                } else if (arrInput[0].equals("unmark")) {
+                    int i = Integer.parseInt(arrInput[1]);
+                    list[i - 1].setStatus(false);
+                    printBox("Task not completed: " + list[i - 1].getTask());
+                } else if (arrInput[0].equals("todo")) {
+                    try {
+                        list[listCount] = new Todo(arrInput[1]);
+                        listCount++;
+                        printBox("Added '" + list[listCount - 1].getTask() + "' into the list.");
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        printBox("O_o Error. The description of Todo cannot be empty. Try again. ");
+                    }
+                } else if (arrInput[0].equals("deadline")) {
+                    try {
+                        String[] dInput = arrInput[1].split("/by ");
+                        list[listCount] = new Deadline(dInput[0], dInput[1]);
+                        listCount++;
+                        printBox("Added '" + list[listCount - 1].getTask() + "' into the list.");
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        printBox("O_o Error. The description of Deadline cannot be empty. Try again. ");
+                    }
+                } else if (arrInput[0].equals("event")) {
+                    try {
+                        String[] eInput = arrInput[1].split("/at ");
+                        list[listCount] = new Event(eInput[0], eInput[1]);
+                        listCount++;
+                        printBox("Added '" + list[listCount - 1].getTask() + "' into the list.");
+                    } catch (ArrayIndexOutOfBoundsException e) {
+                        printBox("O_o Error. The description of Event cannot be empty. Try again. ");
+                    }
+                } else {
+                    throw new DukeException();
+                }
+            } catch(DukeException e) {
+                printBox("O_o Error. I don't understand what that means. Try again.");
             }
         }
-
-        printbox("Bye! See you and take care :)");
+        printBox("See you ^_^");
     }
 }
