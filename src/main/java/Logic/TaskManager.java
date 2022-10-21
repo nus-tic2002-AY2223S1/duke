43d7ef428da8b3125aa33440188b928a4d43a939
denny-wongso <denny.wongso@gmail.com>
2022-10-21 21:23:43 +0800

    move TaskManager to logic package implement BotCallback for router to be able to get the callback added a BotUseCase router to allow linked with other interface implementation

---
+++ src/main/java/Logic/TaskManager.java
@@ -0,0 +1,97 @@
+package Logic;
+
+import Common.TaskType;
+import CustomException.UnsupportedTaskType;
+import Tasks.*;
+import Tasks.TaskInterface;
+import java.util.ArrayList;
+
+public class TaskManager implements BotCallback {
+    ArrayList<TaskInterface> tasks = new ArrayList<>();
+    BotUseCase router;
+
+    public TaskManager(BotUseCase router) {
+        this.router = router;
+        this.router.delegate = this;
+        router.loadData();
+    }
+
+    public void addNewTask(String text) {
+        try {
+            String task = text.substring(0, text.indexOf(' '));
+            String work = text.substring(text.indexOf(" "));
+            TaskType type= TaskType.getType(task);
+            switch (type) {
+                case TODO:
+                    Todo todo = new Todo(work);
+                    tasks.add(todo);
+                    router.addSuccess(todo);
+                    break;
+                case EVENT:
+                    Event event = new Event(work);
+                    tasks.add(event);
+                    router.addSuccess(event);
+                    break;
+                case DEADLINE:
+                    Deadline deadline = new Deadline(work);
+                    tasks.add(deadline);
+                    router.addSuccess(deadline);
+                    break;
+            }
+        } catch (UnsupportedTaskType e) {
+            router.unsupportedTaskType();
+        } catch (IndexOutOfBoundsException e) {
+            router.unsupportedFormat(text);
+        }
+    }
+
+    public void showList() {
+        ArrayList<String> list = new ArrayList<>();
+        int i = 1;
+        for(TaskInterface task: tasks) {
+            list.add(task.getString());
+        }
+        router.showList(tasks);
+    }
+
+    public void markTask(int index, boolean mark) {
+        try {
+            if(tasks.get(index).isDone() == mark) {
+                router.markFailed(tasks.get(index));
+            } else {
+                tasks.get(index).markTask(mark);
+                router.markSuccess(tasks.get(index));
+            }
+        } catch (IndexOutOfBoundsException e) {
+            router.indexOutOFBound();
+        }
+
+    }
+
+    public void remove(int index) {
+        try {
+            TaskInterface task = tasks.get(index);
+            tasks.remove(index);
+            router.deleteSuccess(task);
+        } catch (IndexOutOfBoundsException e)  {
+            router.indexOutOFBound();
+        }
+    }
+
+    public void goodbye() {
+        router.goodbye();
+    }
+
+    @Override
+    public void loadData(ArrayList<TaskInterface> tasks) {
+        this.tasks = tasks;
+    }
+
+    @Override
+    public int getSize() {
+        return tasks.size();
+    }
+
+
+}
+
