b0447bbbf4305870201fd5ac9daae00a0f59b1be
siying_png <siying.png@hotmail.com>
2022-11-01 23:25:11 +0800

    Level 1

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -3,62 +3,44 @@ import java.util.Scanner;
 
 public class Duke {
 
-    protected static String[] Tasks = new String[100];
-    protected static String userReply = "";
-    protected static Scanner in;
-    protected static int countTask = 0;
+    public static class Ui {
 
-    public static void greet() {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
+        private static final String DIVIDER = "____________________________________________________________";
+        private static final String LOGO = " ____        _        \n"
+                                    + "|  _ \\ _   _| | _____ \n"
+                                    + "| | | | | | | |/ / _ \\\n"
+                                    + "| |_| | |_| |   <  __/\n"
+                                    + "|____/ \\__,_|_|\\_\\___|\n";
 
-        String line = " ____________";
-        System.out.println("Hello Siying!\n" + "How's your day today?\n" + line + System.lineSeparator() + "Below are your tasks due today\n" + line );
-        System.out.println("What would you like to do now?");
-    }
-
-    public static void exit() {
-        System.out.println("Bye Siying!" + System.lineSeparator() + "Enjoy your day ;)");
-    }
-
-    public static void getAction() {
-
-        while (!userReply.equals("bye")) {
-            in = new Scanner(System.in);
-            userReply = in.nextLine();
-
-            if (userReply.equals("bye")) {
-                ;
-            } else if (userReply.equals("list")) {
-                printTasks(Tasks);
-            } else {
-                Tasks[countTask] = userReply;
-                countTask++;
-
-                System.out.println(userReply);
-            }
+        public static void greet() {
+            System.out.println("Hello from\n" + LOGO);
+            System.out.println("Hello Siying!\n" + "How's your day today?\n" + DIVIDER + System.lineSeparator() + "Below are your tasks due today\n" + DIVIDER );
+            System.out.println("What would you like to do now?");
         }
-    }
 
-    public static void printTasks(String[] items) {
-        for (int i = 0; i < countTask; i++){
-            System.out.println(i+1 + ". " + items[i]);
+        public static void readInput(){
+            String userInput;
+            Scanner in;
+
+            do {
+                in = new Scanner(System.in);
+                userInput = in.nextLine();
+                System.out.println(userInput);
+            } while (!userInput.equals("bye"));
+            exit();
         }
-    }
 
-//    public static void listDuetoday(){
-//        return Tasks;
-//    }
+        public static void exit() {
+            System.out.println("Bye Siying!" + System.lineSeparator() + "Enjoy your day ;)");
+        }
+
+    }
 
     public static void main(String[] args) {
-
-        greet();
-        getAction();
-        exit();
+        Ui.greet();
+        Ui.readInput();
 
     }
 }
+
+
