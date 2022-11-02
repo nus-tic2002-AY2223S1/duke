9e355f5dafc8570423f0587a664ac9d5cd2088f2
siying_png <siying.png@hotmail.com>
2022-11-02 09:04:46 +0800

    Level 3

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,18 +1,17 @@
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Duke {
-    protected static String[] Tasks = new String[100];
-    private static int countTask = 0;
+    private static final ArrayList<Task> tasklist = new ArrayList<>();
+    protected static int indexTask = 0;
 
     public static class Ui {
-
         private static final String DIVIDER = "____________________________________________________________";
         private static final String LOGO = " ____        _        \n"
-                                    + "|  _ \\ _   _| | _____ \n"
-                                    + "| | | | | | | |/ / _ \\\n"
-                                    + "| |_| | |_| |   <  __/\n"
-                                    + "|____/ \\__,_|_|\\_\\___|\n";
+                + "|  _ \\ _   _| | _____ \n"
+                + "| | | | | | | |/ / _ \\\n"
+                + "| |_| | |_| |   <  __/\n"
+                + "|____/ \\__,_|_|\\_\\___|\n";
 
         public static void greet() {
             System.out.println("Hello from\n" + LOGO);
@@ -30,42 +29,103 @@ public class Duke {
 
                 Parser.identifyCommand(userInput);
             } while (!userInput.equals("bye"));
-            exit();
         }
+
+        public static void printList(){
+            for (int i = 0; i < indexTask; i++) {
+                System.out.println(i + 1 + ".[" + tasklist.get(i).getDone() + "] " + tasklist.get(i).getDescription());
+            }
+        }
+        public static void printTask(int index){
+            System.out.println(" [" + tasklist.get(index).getDone() + "] " + tasklist.get(index).getDescription());
+        }
+
 
         public static void exit() {
             System.out.println("Bye Siying!" + System.lineSeparator() + "Enjoy your day ;)");
         }
-
     }
 
     public static class Parser {
 
         public static void identifyCommand(String input) {
-            switch (input){
+            int descriptionPosition = input.indexOf(" ");
+            String command;
+            String taskDes;
+
+            if (descriptionPosition < 0) {
+                command = input;
+                taskDes = "";
+            } else {
+                command = input.substring(0, descriptionPosition);
+                taskDes = input.substring(descriptionPosition + 1);
+            }
+
+            switch (command) {
                 case "list":
-                    for (int i = 0; i < countTask; i++) {
-                        System.out.println(i+1 + ". " + Tasks[i]);
-                    }
+                    Ui.printList();
+                    break;
+                case "mark":
+                    markCommand(Integer.parseInt(taskDes)-1);
+                    break;
+                case "unmark":
+                    unmarkCommand(Integer.parseInt(taskDes)-1);
                     break;
                 case "bye":
                     break;
                 default:
-                    Tasks[countTask] = input;
-                    countTask++;
-                    System.out.println("added: " + input);
+                    tasklist.add(new Task(input));
+                    indexTask++;
+                    addCommand(input);
                     break;
             }
+        }
+        public static void addCommand(String input){
+            System.out.println("added: " + input);
+        }
+        public static void markCommand(int index){
+            tasklist.get(index).markDone();
+            System.out.println("Nice! I've marked this task as done:");
+            Ui.printTask(index);
+        }
+        public static void unmarkCommand(int index){
+            tasklist.get(index).unmarkDone();
+            System.out.println("OK, I've marked this task as not done yet:");
+            Ui.printTask(index);
+        }
+
+    }
+
+    public static class Task {
+        private String description;
+        private Boolean isDone;
+
+        public Task(String description){
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getDescription(){
+            return this.description;
+        }
+        public String getDone(){
+            return (this.isDone ? "X" : " ");
 
         }
+        public void markDone(){
+            this.isDone = true;
+        }
+        public void unmarkDone(){
+            this.isDone = false;
+        }
+
 
     }
 
     public static void main(String[] args) {
         Ui.greet();
         Ui.readInput();
-
+        Ui.exit();
     }
+
 }
-
-
