285ea83d12e2d289746435c5ab1637d37fcd59ba
lihui <lihui.pineapple@gmail.com>
2022-11-02 20:58:22 +0800

    added TaskMessages.java

---
+++ src/main/java/ui/TaskMessages.java
@@ -0,0 +1,9 @@
+package ui;
+
+public class TaskMessages {
+    public static final String MESSAGE_EXIT = "Bye. Hope to see you again soon!!";
+    public static final String MESSAGE_TASK_ADDED = "Got it. I've added this task: ";
+    public static final String MESSAGE_TASK_REMOVED = "Noted. I've removed this task: ";
+
+
+}
