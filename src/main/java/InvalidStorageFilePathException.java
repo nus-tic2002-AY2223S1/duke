f30916bc84a74ba057436ead4daf661e9967f580
lihui <lihui.pineapple@gmail.com>
2022-11-02 17:33:13 +0800

    moved InvalidStorageFilePathException into exceptions folder

--- src/main/java/InvalidStorageFilePathException.java
+++
@@ -1,5 +0,0 @@
-
-public class InvalidStorageFilePathException extends Exception{
-    public InvalidStorageFilePathException() {super("Error! File Path does not exist. Please check if " + StorageFile.homeDirectory() + "/duke/data/ file path exists.");}
-}
-
