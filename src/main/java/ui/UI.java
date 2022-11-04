1926991d91485bca6ebb6378838e63216b3f9400
lihui <lihui.pineapple@gmail.com>
2022-11-04 15:58:42 +0800

    UI.java: added printMessage and printArray OOB + updated printTask and printBye

--- src/main/java/ui/UI.java
+++ src/main/java/ui/UI.java
@@ -1,10 +1,13 @@
 package ui;
 
-import taskList.Task;
+import java.util.Scanner;
+
+import static ui.ErrorMessages.TASK_NUMBER_OOB;
+import static ui.TaskMessages.*;
 
 public class UI {
 
-    public static void printTask(Task task){
+    public static void printTask(String task){
         System.out.println(task.toString());
     }
 
@@ -20,17 +23,34 @@ public class UI {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
         System.out.println("Hello there! I'm Duke\nWhat can I do for you?");
+        printLine();
     }
 
     public static void printError(String message) {
         System.out.println(message);
     }
-
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
     public static void printStandardError() {
         System.out.println("Sorry, I don't understand what you mean!");
     }
 
+    public static String getUserCommand() {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter Task Action: ");
+        String fullInputLine = in.nextLine();
+        System.out.println("[You have entered:" + fullInputLine + "]");
+        return fullInputLine;
+    }
+
     public static void printBye(){
-        System.out.println("Bye. Hope to see you again soon! ");
+        System.out.println(MESSAGE_EXIT);
+    }
+
+    public static void printArrayOOB(){
+        UI.printLine();
+        UI.printError(TASK_NUMBER_OOB);
+        UI.printLine();
     }
 }
