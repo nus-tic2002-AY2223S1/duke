796364ce44dcbf05e5d3ccf9f299f5e0f9c50825
gigahiga <urouroxd@gmail.com>
2022-11-09 20:23:38 +0800

    Task 1-2 Naufal Urosevic

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,11 +1,36 @@
+import java.util.Scanner;
 public class Duke {
+    private static String[] Addword  = new String[100];
+    private static int WordCounter = 0;
+    private static boolean flag = false;
     public static void main(String[] args) {
-        String logo = " ____        _        \n"
-                + "|  _ \\ _   _| | _____ \n"
-                + "| | | | | | | |/ / _ \\\n"
-                + "| |_| | |_| |   <  __/\n"
-                + "|____/ \\__,_|_|\\_\\___|\n";
-        System.out.println("Hello from\n" + logo);
-        System.out.println("test tag" + logo);
+        System.out.println("Whats up player! What can i help you with today?"+ System.lineSeparator() +"[If you wanna stop playing say bye to me]");
+        Scanner ScanReplies = new Scanner (System.in);
+        String ChatInput;
+        while(!(ChatInput = ScanReplies.nextLine()).equals("bye")) {
+            AnalyzeReplies(ChatInput);
+        }
+        System.out.println("Bye player! Come and fight me again!"); // end the entire Chat
+    }
+    private static void AnalyzeReplies (String ChatInput){
+    if (ChatInput.equals("bye")) { // base case
+        System.out.println("Bye player! Come and fight me again!"); // end the entire Chat
+    }
+    else if (ChatInput.equals("list")){
+            flag = true;
+            ListReplies();
+            flag = false;
+        }
+    else { // considered to be adding into the list
+        Addword[WordCounter] = ChatInput;
+        WordCounter++;
+       }
+    }
+    private static void ListReplies(){
+        if (flag){
+            for (int i = 0; i < Addword.length && Addword[i]!=null; i++) {
+                System.out.println(i+1 + ": " + Addword[i]); // +1 to not allow the starting list number to be 0
+            }
+        }
     }
 }
---
+++ src/main/java/ThrowNewException.java
@@ -0,0 +1,3 @@
+public class ThrowNewException extends Exception{
+
+}
