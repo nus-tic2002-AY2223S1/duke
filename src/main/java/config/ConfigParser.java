5f923118b3296c18694e7c80cafb479c49ad1e5c
bboy-zero <30136511+bboy-zero@users.noreply.github.com>
2022-12-01 10:44:18 +0800

    init setup

---
+++ src/main/java/config/ConfigParser.java
@@ -0,0 +1,16 @@
+package config;
+
+public class ConfigParser {
+
+    // String newLine = System.getProperty("line.separator");
+
+    public static String LOGO_CONST =
+              "  _____    ____       \n"
+            + " |__  /___|  _ \\ ___  \n"
+            + "   / // _ \\ |_) / _ \\ \n"
+            + "  / /|  __/  _ < (_) |\n"
+            + " /____\\___|_| \\_\\___/ \n"
+            + "                      \n";
+
+
+}
